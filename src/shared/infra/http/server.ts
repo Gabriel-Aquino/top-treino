7e8737b272008c0839d4c8020c7e92957cadd519
Gabriel Aquino Castelo Branco <gabrielllpk@gmail.com>
2021-06-28 17:30:34 -0300

    Update server.ts

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'reflect-metadata';
 import cors from 'cors';
 import mainRoute from './routes';
+import '../typeorm';
 
 const app = express();
 app.use(express.json());
