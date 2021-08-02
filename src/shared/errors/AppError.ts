652130ebbed7d8665f59922893e5e1a706962418
Gabriel-Aquino <gabrielllpk@gmail.com>
2021-08-02 17:40:21 -0300

    error handling implementation

--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -1,7 +1,7 @@
 class AppError {
-  public message: string;
+  public readonly message: string;
 
-  public statusCode: number;
+  public readonly statusCode: number;
 
   constructor(message: string, statusCode = 400) {
     this.message = message;
--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,6 +1,8 @@
-import express from 'express';
+import express, { Response, Request, NextFunction } from 'express';
+import 'express-async-errors';
 import 'reflect-metadata';
 import cors from 'cors';
+import AppError from '@shared/errors/AppError';
 import mainRoute from './routes';
 import connection from '../typeorm';
 import '@shared/container';
@@ -12,5 +14,21 @@ app.use(cors());
 app.use(express.json());
 
 app.use(mainRoute);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Integral server error',
+  });
+});
 
 export { app };
